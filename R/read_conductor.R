64a468c5eb0b02f6fd4b6a0d94ea88fd9dc7fbb7
jrealgatius <jordireal@gmail.com>
2022-10-24 14:12:26 +0200

    Arreglat error en read_conductor()

--- R/read_conductor.R
+++ R/read_conductor.R
@@ -1,11 +1,13 @@
-#' Read conductor file different formats txt o rds o xls xlsx o data_frame tibble
-#' @param fitxer Character as a file name and path or data.frame
-#'
-#' @import data.table stringr readxl foreign
-#'
-#' @NoRd
-read_conductor<-function(fitxer,...) {
+#' @title             Read_conductor
+#' @description       Llegeix el fitxer conductor de diferents formats txt o rds o xls xlsx o data_frame tibble
+#' @param fitxer      Fitxer que el convertirem a tibble.
+#' @param ...         Altres funcions
+#' @return            Una taula tibble
+#' @export            read_conductor
+#' @importFrom        dplyr "%>%"
 
+read_conductor<-function(fitxer,...) {
+  # fitxer<-here::here(fitxer_cataleg)
   # Si el fitxer es un data_frame saltar
   if (any(class(fitxer) %in% c("tbl_df","tbl","data.frame")))
 
@@ -15,25 +17,30 @@ read_conductor<-function(fitxer,...) {
 
     if (stringr::str_detect(fitxer,"\\.txt$")){
 
-      dt<-data.table::fread(fitxer) %>% as_tibble()
+      dt<-data.table::fread(fitxer) %>% tibble::as_tibble()
 
     } else if (stringr::str_detect(fitxer,"\\.rds$")) {
 
-      dt<-readRDS(fitxer,...) %>% as_tibble()
+      dt<-readRDS(fitxer,...) %>% tibble::as_tibble()
 
     } else if (stringr::str_detect(fitxer,"\\.xls$")) {
 
-      dt<-readxl::read_excel(fitxer,...) %>% tidyr::as_tibble()
+      dt<-readxl::read_excel(fitxer,...) %>% tibble::as_tibble()
 
     } else if (stringr::str_detect(fitxer,"\\.xlsx$")) {
 
-      dt<-readxl::read_excel(fitxer,...) %>% tidyr::as_tibble()
+      dt<-readxl::read_excel(fitxer,...) %>% tibble::as_tibble()
+
+    } else if (stringr::str_detect(fitxer,"\\.xlS$")) {
+
+      dt<-readxl::read_excel(fitxer,...) %>% tibble::as_tibble()
 
     } else if (stringr::str_detect(fitxer,"\\.sav$")) {
 
-      dt<-foreign::read.spss(fitxer,use.value.labels = T,to.data.frame = T,...) %>% tidyr::as_tibble()
+      dt<-foreign::read.spss(fitxer,use.value.labels = T,to.data.frame = T,...) %>% tibble::as_tibble()
     }
-    else {stop("Data format no reconegut ")}
+    else {stop("format de dades no reconegut ")}
   }
 
 }
+
